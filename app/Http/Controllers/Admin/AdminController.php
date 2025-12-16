01cb585da15b1e94ab31ea84cd9f2ce38f6dc114
Ahsan <ahsanan@outlook.com>
2025-12-16 22:40:54 +0600

    added admin guard

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthAdminRequest;
 use App\Models\Order;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -14,5 +15,35 @@ class AdminController extends Controller
         $yesterDayOrders = Order::whereDay('created_at', Carbon::yesterday())->get();
         $monthOrders = Order::whereMonth('created_at', Carbon::now()->month)->get();
         $yearOrder = Order::whereYear('created_at', Carbon::now()->year)->get();
+
+        return view('admin.index', compact('todayOrders', 'yesterDayOrders', 'monthOrders', 'yearOrder'));
+    }
+
+    public function login() {
+        if(!auth()->guard('admin')->check()) {
+            return view('admin.login');
+        }
+
+        return redirect()->route('admin.index');
+    }
+
+    public function auth(AuthAdminRequest $request) {
+        if($request->validated()) {
+            if(auth()->guard('admin')->attempt([
+                'email' => $request->email,
+                'password' => $request->password])) {
+                $request->session()->regenerate();
+                return redirect()->route('admin.index');
+            }else {
+                return redirect()->route('admin.login')->with([
+                    'error' => 'These credentials do not match our records.',
+                ]);
+            }
+        }
+    }
+
+    public function logout() {
+        auth()->guard('admin')->logout();
+        return redirect()->route('admin.login');
     }
 }

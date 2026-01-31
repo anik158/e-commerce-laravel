14f063fef36f70cf8af9641230d3facc37067d21
Ahsan <ahsanan@outlook.com>
2026-01-31 23:26:55 +0600

    product api is searchable params

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -14,13 +14,40 @@ class ProductController extends Controller
     /**
      * Display a listing of the latest products.
      */
-    public function index()
+    public function index(Request $request)
     {
+        $productQuery = Product::with(['colors', 'sizes', 'reviews']);
 
 
-        return ProductResource::Collection(Product::with(['colors', 'sizes', 'reviews'])->get())->additional([
-            'colors' => Color::has('products')->with('products')->get(),
-            'sizes' => Size::has('products')->with('products')->get(),
+//        Filter by color
+        if($request->has('color') && $request->color !='')
+        {
+            $productQuery->whereHas('colors', function($q) use($request){
+                $q->where('colors.id', $request->color);
+            });
+        }
+
+//        Filter by size
+        if($request->has('size') && $request->size !='')
+        {
+            $productQuery->whereHas('sizes', function($q) use($request){
+                $q->where('sizes.id', $request->size);
+            });
+        }
+
+        if($request->has('search') && trim($request->search) != '')
+        {
+            $productQuery->where(function($q) use($request){
+                $q->where('products.name', 'like', '%'.$request->search.'%');
+            } );
+        }
+
+
+        $products = $productQuery->orderBy('id', 'desc')->get();;
+
+        return ProductResource::collection($products)->additional([
+            'colors' => Color::has('products')->get(['id', 'name']),
+            'sizes'  => Size::has('products')->get(['id', 'name']),
         ]);
 
     }
